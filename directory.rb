8ebc2d4b95687a395fbbbfb2f2ad66e00d78808b
Cal <cal7907@hotmail.co.uk>
2017-11-09 12:05:27 +0000

    Reverted to the default code given at the end of Step 10

--- directory.rb
+++ directory.rb
@@ -1,111 +1,65 @@
-@students = []
+@students = [] # an empty array accessible to all methods
 
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-end
-
-def print_names(names)
-    names.each_with_index do |student, index|
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40)
-        puts "Hobby: #{student[:hobby]}".center(40)
-        puts "Country of birth: #{student[:country]}".center(40)
-    end
-end
-
-def print_footer(names)
-    if names.size > 1
-        puts "Overall, we have #{names.count} great students".center(40)
-    else
-        puts "We only have one student, but they are amazing".center(40)
-    end
-end
-
-# Prompts the user to create the student list
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    name = gets.strip
-    while !name.empty? do
-        puts "What is their hobby?"
-        hobby = gets.strip
-        puts "Where is their country of birth?"
-        country = gets.strip.capitalize
-        puts "Which cohort have they joined?"
-        cohort = gets.strip
-        # gives a default cohort of November if user doesn't enter one
-        cohort = :November if cohort.empty?
-        cohort = cohort.capitalize.to_sym
-        months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-        # checks there are no typos in cohort
-        until months.include?(cohort)
-            puts "Cohort should be the full name of a month. Please check and try again"
-            cohort = gets.strip
-            cohort = cohort.capitalize.to_sym
-        end
-        @students << {name: name, hobby: hobby, country: country, cohort: cohort}
-        puts "Now we have #{@students.count} student#{"s" if @students.size != 1}. Enter a name to add another student, or hit return to exit."
-        name = gets.strip
-    end
-    @students
-end
-
-def show_students
-    print_header
-    print_names(@students)
-    print_footer(@students)
-end
-
-def print_menu
-    puts "Please choose an option by typing one of the following numbers"
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
 end
 
 def interactive_menu
-    loop do
-        print_menu
-        selection = gets.chomp
-        case selection
-        when "1"
-            @students = input_students
-        when "2"
-            show_students
-        when "9"
-            exit
-        else
-            puts "I don't know what you meant. Please enter a number"
-        end
-    end
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
 
 interactive_menu
-
-
-
-# optional methods not used by the main program
-def letter_names(names, letter)
-    names.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[name].start_with? letter
-    end
-end
-
-def length_names(names)
-    names.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
-    end
-end
-
-def select_cohort(names, month)
-    names.select { |student| student[:cohort] == month}
-end
-
-def print_names_while(names)
-    counter = 0
-    while counter < names.length do
-        puts "#{counter + 1}. #{names[counter][:name]} (#{names[counter][:cohort]} cohort)"
-        counter += 1
-    end
-end
